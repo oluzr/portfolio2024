8db7809272ac57166d802ca24a32ea9c3c953580
oluzr <olu-zr@naver.com>
2024-12-26 12:56:57 +0900

    layout: layout

--- src/components/common/BackBtn/BackBtn.tsx
+++ src/components/common/BackBtn/BackBtn.tsx
@@ -1,8 +1,16 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { IoChevronBack } from "react-icons/io5";
 import * as S from "./style";
+import { useState, useEffect } from "react";
 const BackBtn = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [canGoBack, setCanGoBack] = useState(false);
+  useEffect(() => {
+    console.log(location.key);
+    setCanGoBack(location.key!= 'default');
+  }, [location.key]);
+  if (!canGoBack) return <div></div>;
   return (
     <S.BackBtnStyleContainer onClick={() => navigate(-1)}>
       <IoChevronBack />
